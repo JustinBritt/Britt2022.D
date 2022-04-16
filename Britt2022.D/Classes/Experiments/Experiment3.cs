42f76a6bad8a0ca61dd0fed1413585c128d4a2ca
Justin Britt <britt@uwindsor.ca>
2022-04-16 08:26:11 -0400

    Added class

---
+++ Britt2022.D/Classes/Experiments/Experiment3.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.D.Classes.Experiments
+{
+    using Britt2022.D.Interfaces.Experiments;
+
+    internal sealed class Experiment3 : IExperiment3
+    {
+        public Experiment3()
+        {
+        }
+    }
+}
---
+++ Britt2022.D/Interfaces/Experiments/IExperiment3.cs
@@ -0,0 +1,10 @@
+﻿namespace Britt2022.D.Interfaces.Experiments
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IExperiment3
+    {
+    }
+}
