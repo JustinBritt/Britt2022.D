7f19776e09446c8ac5aebacde48d64010fb854ff
Justin Britt <britt@uwindsor.ca>
2022-04-16 08:48:15 -0400

    Added test class

---
+++ Britt2022.D.Tests/Classes/Calculations/DCalculation.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.D.Tests.Classes.Calculations
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class DCalculation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
