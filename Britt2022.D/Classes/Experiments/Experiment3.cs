ca972f325902e2668164eb84309e80d52ca3f933
Justin Britt <britt@uwindsor.ca>
2022-04-16 12:14:54 -0400

    Added logging

--- Britt2022.D/Classes/Experiments/Experiment3.cs
+++ Britt2022.D/Classes/Experiments/Experiment3.cs
@@ -1,9 +1,13 @@
 ﻿namespace Britt2022.D.Classes.Experiments
 {
+    using log4net;
+
     using Britt2022.D.Interfaces.Experiments;
 
     internal sealed class Experiment3 : IExperiment3
     {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public Experiment3()
         {
         }
