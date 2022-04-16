d56e6e51703e0b7b0d16a408582c1877ae2c0e72
Justin Britt <britt@uwindsor.ca>
2022-04-16 12:49:13 -0400

    Added test class

---
+++ Britt2022.D.Tests/Classes/Experiments/Experiment1.cs
@@ -0,0 +1,13 @@
+﻿namespace Britt2022.D.Tests.Classes.Experiments
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class Experiment1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
