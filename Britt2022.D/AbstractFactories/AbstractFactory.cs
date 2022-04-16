079c226c0e747294fce64e6d5d2cccbbc0bfd4fb
Justin Britt <britt@uwindsor.ca>
2022-04-16 10:11:37 -0400

    Added abstract factory

---
+++ Britt2022.D/AbstractFactories/AbstractFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace Britt2022.D.AbstractFactories
+{
+    using Britt2022.D.AbstractFactories;
+    using Britt2022.D.InterfacesAbstractFactories;
+
+    public sealed class AbstractFactory : IAbstractFactory
+    {
+        public AbstractFactory()
+        {
+        }
+
+        public static IAbstractFactory Create()
+        {
+            return new AbstractFactory();
+        }
+
+        public ICalculationsAbstractFactory CreateCalculationsAbstractFactory()
+        {
+            ICalculationsAbstractFactory abstractFactory = null;
+
+            try
+            {
+                abstractFactory = new CalculationsAbstractFactory();
+            }
+            finally
+            {
+            }
+
+            return abstractFactory;
+        }
+
+        public IDependenciesAbstractFactory CreateDependenciesAbstractFactory()
+        {
+            IDependenciesAbstractFactory abstractFactory = null;
+
+            try
+            {
+                abstractFactory = new DependenciesAbstractFactory();
+            }
+            finally
+            {
+            }
+
+            return abstractFactory;
+        }
+    }
+}
---
+++ Britt2022.D/InterfacesAbstractFactories/IAbstractFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Britt2022.D.InterfacesAbstractFactories
+{
+    public interface IAbstractFactory
+    {
+        ICalculationsAbstractFactory CreateCalculationsAbstractFactory();
+
+        IDependenciesAbstractFactory CreateDependenciesAbstractFactory();
+    }
+}
