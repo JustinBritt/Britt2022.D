d40f40a3c012ef33486b99485bc2f3e65741730c
Justin Britt <britt@uwindsor.ca>
2022-04-17 04:39:30 -0400

    Added abstract factory

---
+++ Britt2022.D/AbstractFactories/ExperimentsAbstractFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace Britt2022.D.AbstractFactories
+{
+    using Britt2022.D.Factories.Experiments;
+    using Britt2022.D.InterfacesAbstractFactories;
+    using Britt2022.D.InterfacesFactories.Experiments;
+
+    internal sealed class ExperimentsAbstractFactory : IExperimentsAbstractFactory
+    {
+        public ExperimentsAbstractFactory()
+        {
+        }
+
+        public IExperiment1Factory CreateExperiment1Factory()
+        {
+            IExperiment1Factory factory = null;
+
+            try
+            {
+                factory = new Experiment1Factory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public IExperiment3Factory CreateExperiment3Factory()
+        {
+            IExperiment3Factory factory = null;
+
+            try
+            {
+                factory = new Experiment3Factory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ Britt2022.D/InterfacesAbstractFactories/IExperimentsAbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Britt2022.D.InterfacesAbstractFactories
+{
+    using Britt2022.D.InterfacesFactories.Experiments;
+
+    public interface IExperimentsAbstractFactory
+    {
+        IExperiment1Factory CreateExperiment1Factory();
+
+        IExperiment3Factory CreateExperiment3Factory();
+    }
+}
