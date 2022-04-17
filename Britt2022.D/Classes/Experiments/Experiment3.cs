7b42154baec05e1cd1d827eaeaf5f8c13d5559f4
Justin Britt <britt@uwindsor.ca>
2022-04-17 05:19:34 -0400

    Added property; Changes to usings

--- Britt2022.D/Classes/Experiments/Experiment3.cs
+++ Britt2022.D/Classes/Experiments/Experiment3.cs
@@ -1,8 +1,20 @@
 ﻿namespace Britt2022.D.Classes.Experiments
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+
     using log4net;
 
+    using Hl7.Fhir.Model;
+
+    using Britt2022.D.Extensions.Dependencies.Hl7.Fhir.R4.Model;
+    using Britt2022.D.Interfaces.Calculations;
     using Britt2022.D.Interfaces.Experiments;
+    using Britt2022.D.InterfacesAbstractFactories;
+    using Britt2022.D.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using Britt2022.D.InterfacesFactories.Dependencies.MathNet.Numerics.Distributions;
 
     internal sealed class Experiment3 : IExperiment3
     {
@@ -11,5 +23,10 @@
         public Experiment3()
         {
         }
+
+        /// <summary>
+        /// Gets a factory can create FhirBoolean, FhirDecimal, and PositiveInt instances.
+        /// </summary>
+        public INullableValueFactory NullableValueFactory { get; }
     }
 }
