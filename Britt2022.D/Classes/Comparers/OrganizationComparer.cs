31b92825b6e284c08c83666ef7ed7b58e1ab6bab
Justin Britt <britt@uwindsor.ca>
2023-06-01 11:57:27 -0400

    Added class

---
+++ Britt2022.D/Classes/Comparers/OrganizationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2022.D.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.D.Interfaces.Comparers;
+
+    internal sealed class OrganizationComparer : IOrganizationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public OrganizationComparer()
+        {
+        }
+
+        public int Compare(
+                Organization x,
+                Organization y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
