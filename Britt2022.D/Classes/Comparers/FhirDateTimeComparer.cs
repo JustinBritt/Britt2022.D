b718c33a15dcc59e4574e878836fcfdd3e93b145
Justin Britt <britt@uwindsor.ca>
2023-06-01 12:41:59 -0400

    Added class

---
+++ Britt2022.D/Classes/Comparers/FhirDateTimeComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2022.D.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.D.Interfaces.Comparers;
+
+    internal sealed class FhirDateTimeComparer : IFhirDateTimeComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public FhirDateTimeComparer()
+        {
+        }
+
+        public int Compare(
+                FhirDateTime x,
+                FhirDateTime y)
+        {
+            return x.ToDateTimeOffset(TimeSpan.Zero).UtcDateTime
+                .CompareTo(
+                y.ToDateTimeOffset(TimeSpan.Zero).UtcDateTime);
+        }
+    }
+}
