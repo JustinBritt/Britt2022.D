59825625f995217386f57870920afb2ab16430ac
Justin Britt <britt@uwindsor.ca>
2023-06-01 12:54:45 -0400

    Added factory

---
+++ Britt2022.D/Factories/Comparers/FhirDateTimeComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Britt2022.D.Factories.Comparers
+{
+    using Britt2022.D.Classes.Comparers;
+    using Britt2022.D.Interfaces.Comparers;
+    using Britt2022.D.InterfacesFactories.Comparers;
+
+    internal sealed class FhirDateTimeComparerFactory : IFhirDateTimeComparerFactory
+    {
+        public FhirDateTimeComparerFactory()
+        {
+        }
+
+        public IFhirDateTimeComparer Create()
+        {
+            IFhirDateTimeComparer instance = null;
+
+            try
+            {
+                instance = new FhirDateTimeComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
