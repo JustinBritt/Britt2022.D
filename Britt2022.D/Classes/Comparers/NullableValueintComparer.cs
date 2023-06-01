8878d05918620753c8facd83dfc9b4e426d8bfe4
Justin Britt <britt@uwindsor.ca>
2023-06-01 11:21:02 -0400

    Added class

---
+++ Britt2022.D/Classes/Comparers/NullableValueintComparer.cs
@@ -0,0 +1,25 @@
+﻿namespace Britt2022.D.Classes.Comparers
+{
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Britt2022.D.Interfaces.Comparers;
+
+    internal sealed class NullableValueintComparer : INullableValueintComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public NullableValueintComparer()
+        {
+        }
+
+        public int Compare(
+                INullableValue<int> x,
+                INullableValue<int> y)
+        {
+            return x.Value.Value.CompareTo(
+                y.Value.Value);
+        }
+    }
+}
---
+++ Britt2022.D/Interfaces/Comparers/INullableValueintComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace Britt2022.D.Interfaces.Comparers
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    public interface INullableValueintComparer : IComparer<INullableValue<int>>
+    {
+    }
+}
