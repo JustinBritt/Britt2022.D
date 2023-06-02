ab5882a60cb9f0fd461429360c3b286442a9dfdd
Justin Britt <britt@uwindsor.ca>
2023-06-02 11:44:25 -0400

    Use sorted set instead of list

--- Britt2022.D/Interfaces/Calculations/IpCalculation.cs
+++ Britt2022.D/Interfaces/Calculations/IpCalculation.cs
@@ -15,7 +15,7 @@
             INullableValueFactory nullableValueFactory,
             IDiscreteUniformFactory discreteUniformFactory,
             ImmutableList<PositiveInt> lengthOfStayDays,
-            ImmutableList<INullableValue<int>> scenarios,
+            ImmutableSortedSet<INullableValue<int>> scenarios,
             Organization surgeon,
             ImmutableList<KeyValuePair<Organization, INullableValue<int>>> surgeonLengthOfStayMaximums,
             double targetMean);
