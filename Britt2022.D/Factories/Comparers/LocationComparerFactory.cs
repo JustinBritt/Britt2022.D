99e8de9b2078a0aa9bb9e4cd858e88d74bdca408
Justin Britt <britt@uwindsor.ca>
2023-06-02 14:28:33 -0400

    Added factory

--- Britt2022.D/Factories/Comparers/LocationComparerFactory.cs
+++ Britt2022.D/Factories/Comparers/LocationComparerFactory.cs
@@ -1,6 +1,28 @@
 ﻿namespace Britt2022.D.Factories.Comparers
 {
-    internal sealed class LocationComparerFactory
+    using Britt2022.D.Classes.Comparers;
+    using Britt2022.D.Interfaces.Comparers;
+    using Britt2022.D.InterfacesFactories.Comparers;
+
+    internal sealed class LocationComparerFactory : ILocationComparerFactory
     {
+        public LocationComparerFactory()
+        {
+        }
+
+        public ILocationComparer Create()
+        {
+            ILocationComparer instance = null;
+
+            try
+            {
+                instance = new LocationComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
     }
 }
